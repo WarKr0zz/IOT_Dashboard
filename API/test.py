b9548bd21ed102b6a8de2e3e36ebf886ad3f166b
WarKr0Zz <econte0108@gmail.com>
2022-12-14 20:18:41 +0100

    testwebscraping selenium

--- API/test.py
+++ API/test.py
@@ -2,6 +2,7 @@ from ast import Bytes
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -10,8 +11,16 @@ opts.add_argument("user-agent=Mozilla")
 
 DRIVER_PATH = 'D:\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH,chrome_options=opts)
-driver.get('http://localhost/IOT_Dashboard/API/DeviceId/1')
+driver.get('http://localhost/IOT_Dashboard/API/')
+elem = driver.find_elements(By.ID,'WebScrapping')
 
+
+if elem == "Array ( [message] => Probleme de récupération de données. [code] => 0 )": 
+    print("OK")
+else:
+    print("Pas OK")
+
+# check if the innerHTML is empty
 
 
 
