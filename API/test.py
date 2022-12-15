e73fa16cdf3520bb7dbfa0c528dd7654c24ad980
WarKr0Zz <econte0108@gmail.com>
2022-12-15 09:30:13 +0100

    test selenium fini

--- API/test.py
+++ API/test.py
@@ -10,19 +10,29 @@ opts = Options()
 opts.add_argument("user-agent=Mozilla")
 
 DRIVER_PATH = 'D:\chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH,chrome_options=opts)
-driver.get('http://localhost/IOT_Dashboard/API/')
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.get('http://localhost/IOT_Dashboard/API')
 elem = driver.find_elements(By.ID,'WebScrapping')
 
 
-if elem == "Array ( [message] => Probleme de récupération de données. [code] => 0 )": 
-    print("OK")
-else:
-    print("Pas OK")
+test1 = elem[0].text
+print("test page principal")
+if test1 == "Array ( [message] => Probleme de récupération de données. [code] => 0 )" :
+    print("OK tout est bon")
+else : 
+    print("pas OKdu tout")
 
-# check if the innerHTML is empty
 
 
+## test 2
+driver.get('http://localhost/IOT_Dashboard/API/DeviceId')
+elem2 = driver.find_elements(By.ID,'WebScrapping')
+test2 = elem2[0].text
+print("test numero 2 liste des device")
+if len(test2) > 4 :
+    print("OK tout est bon")
+else : 
+    print("pas OKdu tout")
 
 while True:
     pass
